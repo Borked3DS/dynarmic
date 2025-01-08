03b4a23bedc9cb0f0dccdd171ae11dd2ac7de8c9
Miguel <miguelopendeveloper@proton.me>
2025-01-07 22:47:01 -0700

    identity_removal_pass.cpp: Use kvec instead of std::vector

--- src/dynarmic/ir/opt/identity_removal_pass.cpp
+++ src/dynarmic/ir/opt/identity_removal_pass.cpp
@@ -3,40 +3,48 @@
  * SPDX-License-Identifier: 0BSD
  */
 
-#include <vector>
+#include <cstdlib>
 
 #include "dynarmic/ir/basic_block.h"
 #include "dynarmic/ir/opcodes.h"
 #include "dynarmic/ir/opt/passes.h"
+#include "kvec.h"
 
 namespace Dynarmic::Optimization {
 
 void IdentityRemovalPass(IR::Block& block) {
-    std::vector<IR::Inst*> to_invalidate;
+    kvec_t(IR::Inst*) to_invalidate;
+    kv_init(to_invalidate);
 
     for (auto& inst : block) {
         const size_t num_args = inst.NumArgs();
-        std::vector<IR::Value> args(num_args);
+        kvec_t(IR::Value) args;
+        kv_init(args);
 
         for (size_t i = 0; i < num_args; ++i) {
-            args[i] = inst.GetArg(i);
-            if (args[i].IsIdentity()) {
-                args[i] = args[i].GetInst()->GetArg(0);
+            IR::Value arg = inst.GetArg(i);
+            if (arg.IsIdentity()) {
+                arg = arg.GetInst()->GetArg(0);
             }
+            kv_push(IR::Value, args, arg);
         }
 
         for (size_t i = 0; i < num_args; ++i) {
-            inst.SetArg(i, args[i]);
+            inst.SetArg(i, kv_A(args, i));
         }
 
         if (inst.GetOpcode() == IR::Opcode::Identity || inst.GetOpcode() == IR::Opcode::Void) {
-            to_invalidate.push_back(&inst);
+            kv_push(IR::Inst*, to_invalidate, &inst);
         }
+
+        kv_destroy(args);
     }
 
-    for (IR::Inst* inst : to_invalidate) {
-        inst->Invalidate();
+    for (size_t i = 0; i < kv_size(to_invalidate); i++) {
+        to_invalidate.a[i]->Invalidate();
     }
+
+    kv_destroy(to_invalidate);
 }
 
 }  // namespace Dynarmic::Optimization
